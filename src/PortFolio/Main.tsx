5cc62a8ee1e0fe44dfbaba65e140b8af8241099a
hjzhhhj <jhj090120@gmail.com>
2025-05-20 12:05:37 +0900

    프레이머 모션 사용

--- src/PortFolio/Main.tsx
+++ src/PortFolio/Main.tsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 import "./Main.css"
 import TypingEffect from "../Component/TypingEffect"
 
-function Main () {
+function Main() {
     return (
-        <div className="All">
-            <div className="top-panel">
-                {/* <Toppanel /> */}
+        <motion.div
+            initial={{ opacity: 0 }}        
+            animate={{ opacity: 1 }}        
+            transition={{ duration: 3 }}   
+            >
+            <div className="All">
+                <div className="top-panel">
+                    {/* <Toppanel /> */}
+                </div>
+                <div className="introduce">
+                    <p className="Hi">안녕하세요!</p>
+                    <p><span className="Typing"><TypingEffect /> 개발자, 정희진입니다.</span></p>
+                    <p className="motto">네가 세상에서 보고 싶은 변화가 되어라 – 마하트마 간디</p>
+                </div>
             </div>
-            <div className="introduce">
-                <p className="Hi">안녕하세요!</p>
-                <p><span className="Typing"><TypingEffect /> 개발자, 정희진입니다.</span></p>
-                <p className="motto">네가 세상에서 보고 싶은 변화가 되어라 – 마하트마 간디</p>
-            </div>
-        </div>
+        </motion.div>
     )
 }
 
