8a7107207c1c30c455b304f54c3e466049c774d3
hjzhhhj <jhj090120@gmail.com>
2025-06-24 13:03:37 +0900

    [FIX] : Introduce 프레이머 모션 수정

--- src/pages/Introduce.tsx
+++ src/pages/Introduce.tsx
@@ -105,7 +105,7 @@ function Introduce() {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.5 }}
+          transition={{ duration: 0.5, delay: 0.7 }}
         >
           <MainText>
             <TypingEffect />&nbsp;개발자, 정희진입니다
